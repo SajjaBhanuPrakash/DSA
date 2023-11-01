af70139a75c2a34435adac28b721ea81df29924b
SajjaBhanuPrakash <65525372+SajjaBhanuPrakash@users.noreply.github.com>
2023-11-01 20:27:27 +0000

    Initial commit

---
+++ findNemo.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void findNemo(String[] array) {
+    for(int i=0;i < array.length; i++) {
+      if(array[i].equals("nemo") ) {
+        System.out.println("FoundNemo");
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    final String[] nemo = {"nemo"};
+    findNemo(nemo);
+
+  }
+}
